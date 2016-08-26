ade37141c60c344cfe8a7e1fb57c7ea1e21fe6b4
shiraji <isogai.shiraji@gmail.com>
2016-08-26 16:31:08 +0900

    update it

---
+++ src/main/kotlin/com/github/shiraji/colorfinder/view/ColorFinderToolWindowItem.java
@@ -0,0 +1,8 @@
+package com.github.shiraji.colorfinder.view;
+
+import javax.swing.*;
+
+public class ColorFinderToolWindowItem {
+    JPanel panel;
+    JLabel colorName;
+}
--- src/main/kotlin/com/github/shiraji/colorfinder/view/ColorFinderToolWindowPanel.kt
+++ src/main/kotlin/com/github/shiraji/colorfinder/view/ColorFinderToolWindowPanel.kt
@@ -6,12 +6,38 @@ import com.intellij.openapi.actionSystem.DataProvider
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.SimpleToolWindowPanel
+import com.intellij.openapi.util.JDOMUtil
+import com.intellij.psi.search.FilenameIndex
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.ui.ScrollPaneFactory
+import com.intellij.ui.components.JBList
 import com.intellij.util.ui.JBUI
+import java.awt.Color
+import javax.swing.DefaultListModel
 import javax.swing.JPanel
 
 class ColorFinderToolWindowPanel(val project: Project) : SimpleToolWindowPanel(false, true), DataProvider, Disposable {
 
     init {
+        val colorMap = mutableMapOf<String, String>()
+        FilenameIndex.getFilesByName(project, "colors.xml", GlobalSearchScope.projectScope(project)).forEach {
+            colorFile ->
+            JDOMUtil.loadDocument(colorFile.text).rootElement.getChildren("color").forEach {
+                colorElement ->
+                colorElement.getAttribute("name").value?.let {
+                    colorMap.put(it, colorElement.text)
+                }
+            }
+        }
+        val listModel = DefaultListModel<ColorFinderToolWindowItem>()
+        colorMap.forEach {
+            val item = ColorFinderToolWindowItem()
+            item.colorName.text = it.key
+            item.panel.background = Color.decode(it.value)
+            listModel.addElement(item)
+        }
+        val list = JBList(listModel)
+        setContent(ScrollPaneFactory.createScrollPane(list))
         setToolbar(createToolbarPanel())
     }
 
