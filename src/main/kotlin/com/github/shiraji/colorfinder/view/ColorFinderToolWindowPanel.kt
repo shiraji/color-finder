a315f2bf1a722dc6542803f50a7bdcf4ee6cf76d
shiraji <isogai.shiraji@gmail.com>
2016-08-27 16:31:49 +0900

    Update a lot

--- src/main/kotlin/com/github/shiraji/colorfinder/view/ColorFinderToolWindowPanel.kt
+++ src/main/kotlin/com/github/shiraji/colorfinder/view/ColorFinderToolWindowPanel.kt
@@ -3,6 +3,8 @@ package com.github.shiraji.colorfinder.view
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.SimpleToolWindowPanel
 import com.intellij.openapi.util.JDOMUtil
@@ -12,14 +14,18 @@ import com.intellij.ui.ListSpeedSearch
 import com.intellij.ui.ScrollPaneFactory
 import com.intellij.ui.components.JBList
 import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.TextTransferable
 import java.awt.Color
 import java.awt.Component
-import javax.swing.DefaultListCellRenderer
-import javax.swing.DefaultListModel
-import javax.swing.JList
-import javax.swing.JPanel
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import javax.swing.*
 
 class ColorFinderToolWindowPanel(val project: Project) : SimpleToolWindowPanel(false, true), DataProvider, Disposable {
+
+    val listModel: DefaultListModel<String> by lazy {
+        DefaultListModel<String>()
+    }
 
     init {
         setContent()
@@ -27,17 +33,7 @@ class ColorFinderToolWindowPanel(val project: Project) : SimpleToolWindowPanel(f
     }
 
     private fun setContent() {
-        val colorMap = mutableMapOf<String, String>()
-        FilenameIndex.getFilesByName(project, "colors.xml", GlobalSearchScope.projectScope(project)).forEach {
-            colorFile ->
-            JDOMUtil.loadDocument(colorFile.text).rootElement.getChildren("color").forEach {
-                colorElement ->
-                colorElement.getAttribute("name").value?.let {
-                    colorMap.put("R.color.$it", colorElement.text)
-                }
-            }
-        }
-        val listModel = DefaultListModel<String>()
+        val colorMap = createColorMap()
         colorMap.forEach {
             listModel.addElement(it.key)
         }
@@ -58,24 +54,65 @@ class ColorFinderToolWindowPanel(val project: Project) : SimpleToolWindowPanel(f
         }
         // Google recommended height!!!
         list.fixedCellHeight = 48
-
         ListSpeedSearch(list)
+
+        list.addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent?) {
+                super.mouseClicked(e)
+                if (!SwingUtilities.isRightMouseButton(e)) return
+                if (list.isEmpty) return
+                e ?: return
+
+                val selectedColor = listModel.get(list.minSelectionIndex)
+                val copyMenu = JMenuItem("Copy $selectedColor").apply {
+                    addActionListener {
+                        CopyPasteManager.getInstance().setContents(TextTransferable(selectedColor))
+                    }
+                }
+
+                JPopupMenu().run {
+                    add(copyMenu)
+                    show(e.component, e.x, e.y)
+                }
+            }
+        })
+
         setContent(ScrollPaneFactory.createScrollPane(list))
     }
 
     private fun createToolbarPanel(): JPanel {
         val group = DefaultActionGroup()
         group.add(RefreshAction())
-        val actionToolBar = ActionManager.getInstance().createActionToolbar("ColorFinder", group, true)
+        val actionToolBar = ActionManager.getInstance().createActionToolbar("ColorFinder", group, false)
         return JBUI.Panels.simplePanel(actionToolBar.component)
     }
 
     override fun dispose() {
     }
 
+    private fun createColorMap(): Map<String, String> {
+        val colorMap = mutableMapOf<String, String>()
+        FilenameIndex.getFilesByName(project, "colors.xml", GlobalSearchScope.projectScope(project)).forEach {
+            colorFile ->
+            JDOMUtil.loadDocument(colorFile.text).rootElement.getChildren("color").forEach {
+                colorElement ->
+                colorElement.getAttribute("name").value?.let {
+                    colorMap.put("R.color.$it", colorElement.text)
+                }
+            }
+        }
+        return colorMap
+    }
+
     inner class RefreshAction() : AnAction("Reload colors.xml", "Reload colors.xml", AllIcons.Actions.Refresh) {
         override fun actionPerformed(e: AnActionEvent?) {
-            setContent()
+            ApplicationManager.getApplication().runWriteAction {
+                val colorMap = createColorMap()
+                listModel.removeAllElements()
+                colorMap.forEach {
+                    listModel.addElement(it.key)
+                }
+            }
         }
     }
 }
